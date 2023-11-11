214f9b934bc86c59900920615e3128acf1b7cebc
Gonzalo Romero <gonzalo.romeroe@edu.uah.es>
2023-11-11 20:06:18 +0100

    init frontend microservice

---
+++ peliculas-front/src/main/java/es/uah/peliculasfront/PeliculasFrontApplication.java
@@ -0,0 +1,13 @@
+package es.uah.peliculasfront;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeliculasFrontApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeliculasFrontApplication.class, args);
+	}
+
+}
---
+++ peliculas-front/src/test/java/es/uah/peliculasfront/PeliculasFrontApplicationTests.java
@@ -0,0 +1,13 @@
+package es.uah.peliculasfront;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeliculasFrontApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
